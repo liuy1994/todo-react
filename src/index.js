abb58fc045aa75c098459245e73edc8856358011
liuy1994 <liuy1994@outlook.com>
2017-10-15 08:59:52 +0800

    fist-update

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>Ho, world!</h1>,document.getElementById('root')
+  );
 registerServiceWorker();
+
+
